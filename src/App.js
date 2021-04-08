81bf8a4af25e6f942f27f279813860fa46e4575e
Eli-b-good <eli.i.henderson95@gmail.com>
2021-04-07 23:16:42 -0400

    Styling setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+import { FaTwitter, FaLinkedin, FaCheck } from "react-icons/fa";
+import { ImCross } from "react-icons/im";
+
+const App = () => {
+  const [twitterAuthed, setTwitterAuthed] = useState(false);
+  const [linkedinAuthed, setLinkedInAuth] = useState(false);
+  const [user, setUser] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="header">
+        <div className="multi">
+          <p>Dual</p>
+        </div>
+        <div className="post">
+          <p>Post</p>
+        </div>
+      </div>
+
+      {user ? (
+        <div className="post-container">
+          <h1>Post Form</h1>
+        </div>
+      ) : (
+        <div className="auth-container">
+          <div className="twitter-container">
+            <div id="twitter-div">
+              <FaTwitter /> <hr /> <p>Sign in with Twitter</p>
+            </div>
+            <div id="authed">
+              {twitterAuthed ? (
+                <FaCheck style={{ color: "#41e641" }} />
+              ) : (
+                <ImCross style={{ color: "#ff3c3c" }} />
+              )}
+            </div>
+          </div>
+          <div id="break"></div>
+          <div className="linkedin-container">
+            <div id="linkedin-div">
+              <FaLinkedin /> <hr /> <p>Sign in with LinkedIn</p>
+            </div>
+
+            <div id="authed">
+              {linkedinAuthed ? (
+                <FaCheck style={{ color: "#41e641" }} />
+              ) : (
+                <ImCross style={{ color: "#ff3c3c" }} />
+              )}
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
