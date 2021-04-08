a02bbca1c87f68de6360b289028e906cd3ae9b34
Eli-b-good <eli.i.henderson95@gmail.com>
2021-04-08 01:52:11 -0400

    data successfully being sent from both profiles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 
 import { FaTwitter, FaLinkedin, FaCheck } from "react-icons/fa";
@@ -7,7 +8,63 @@ import { ImCross } from "react-icons/im";
 const App = () => {
   const [twitterAuthed, setTwitterAuthed] = useState(false);
   const [linkedinAuthed, setLinkedInAuth] = useState(false);
-  const [user, setUser] = useState(false);
+  const [twitterUserData, setTwitterData] = useState({});
+  const [linkedInData, setLinkedInData] = useState({});
+
+  useEffect(() => {
+    async function getTwitterUser() {
+      let twitterUser = await axios.get(
+        "http://localhost:3001/twitter/getuser"
+      );
+      const response = await twitterUser;
+
+      if (response.data.twitterUser !== null) {
+        setTwitterAuthed(true);
+      }
+
+      setTwitterData(response.data);
+    }
+    getTwitterUser();
+
+    async function getLinkedInUser() {
+      let linkedInUser = await axios.get(
+        "http://localhost:3001/linkedin/getuser"
+      );
+      const res = await linkedInUser;
+
+      console.log(res);
+      if (res.data.linkedInUser !== null) {
+        setLinkedInAuth(true);
+      }
+
+      setLinkedInData(res.data);
+    }
+    getLinkedInUser();
+  }, []);
+
+  console.log(twitterUserData);
+  console.log(linkedInData);
+
+  const sendPost = async (e) => {
+    e.preventDefault();
+    const linkedInResponse = await axios.get(
+      "http://localhost:3001/linkedin/share"
+    );
+    const twitterResponse = await axios.post(
+      "http://localhost:3001/twitter/post"
+    );
+
+    console.log(twitterResponse);
+    console.log(linkedInResponse);
+  };
+
+  const logout = async (e) => {
+    e.preventDefault();
+    await axios.get("http://localhost:3001/logout");
+    setLinkedInAuth(false);
+    setTwitterAuthed(false);
+  };
+
   return (
     <div className="app">
       <div className="header">
@@ -19,16 +76,24 @@ const App = () => {
         </div>
       </div>
 
-      {user ? (
+      {twitterAuthed && linkedinAuthed ? (
         <div className="post-container">
-          <h1>Post Form</h1>
+          <form className="post-form" onSubmit={sendPost}>
+            <input type="text" />
+            <input type="text" />
+            <button type="submit">Dual Post</button>
+          </form>
+          <button onClick={logout}>logout</button>
         </div>
       ) : (
         <div className="auth-container">
           <div className="twitter-container">
-            <div id="twitter-div">
-              <FaTwitter /> <hr /> <p>Sign in with Twitter</p>
-            </div>
+            <a href="http://localhost:3001/auth/twitter/login">
+              <div id="twitter-div">
+                <FaTwitter /> <hr /> <p>Sign in with Twitter</p>
+              </div>
+            </a>
+
             <div id="authed">
               {twitterAuthed ? (
                 <FaCheck style={{ color: "#41e641" }} />
@@ -39,9 +104,11 @@ const App = () => {
           </div>
           <div id="break"></div>
           <div className="linkedin-container">
-            <div id="linkedin-div">
-              <FaLinkedin /> <hr /> <p>Sign in with LinkedIn</p>
-            </div>
+            <a href="http://localhost:3001/auth/linkedin/login">
+              <div id="linkedin-div">
+                <FaLinkedin /> <hr /> <p>Sign in with LinkedIn</p>
+              </div>
+            </a>
 
             <div id="authed">
               {linkedinAuthed ? (
