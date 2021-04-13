08c7d137f5948f624c91c9ee0c991d664e48886d
Eli-b-good <eli.i.henderson95@gmail.com>
2021-04-13 03:05:23 -0400

    STYLING: header and login container mobile styling

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,10 +1,26 @@
 import React from "react";
 
+import { FaTwitter, FaLinkedin } from "react-icons/fa";
+
+import "./LoginPageStyles.css";
 const LoginPage = () => {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <>
+      <div className="login-container">
+        <h3>Please login with both Twitter and LinkedIn</h3>
+        <div className="twitter-button">
+          <FaTwitter id="fa-twitter" />
+          <hr />
+          <p>Login with Twitter</p>
+        </div>
+        <div className="login-divider"></div>
+        <div className="linkedin-button">
+          <FaLinkedin id="fa-linkedin" />
+          <hr />
+          <p>Login with LinkedIn</p>
+        </div>
+      </div>
+    </>
   );
 };
 
